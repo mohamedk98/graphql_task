34300b310fc42c83a820b95708d791f84e5d58a2
mohamedk98 <mohamedk98498@gmail.com>
2022-07-03 11:53:33 +0200

    initial commit

---
+++ .history/app_20220703114519.js
@@ -0,0 +1,9 @@
+const {ApolloServer} = require("apollo-server")
+const server = new ApolloServer({})
+
+
+
+
+
+
+server.listen()
---
+++ .history/app_20220703114839.js
@@ -0,0 +1,40 @@
+const { ApolloServer, gql } = require("apollo-server");
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+server.listen().then(()=>{
+    console.log("Graphql server is working")
+});
